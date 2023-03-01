9f2884ffe0c96cd3ef2a57a4283ef65ae8ab4b91
ievsepl <107007762+ievsepl@users.noreply.github.com>
2023-03-01 19:58:24 +0200

    add ch

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { useCallback, useEffect } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-// import * as Font from 'expo-font';
-import {
-  StyleSheet,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableWithoutFeedback,
-  Keyboard,
-  ImageBackground,
-  Dimensions,
-} from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { useCallback, useEffect } from 'react';
+// import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+// import {
+//   StyleSheet,
+//   View,
+//   KeyboardAvoidingView,
+//   Platform,
+//   TouchableWithoutFeedback,
+//   Keyboard,
+//   ImageBackground,
+//   Dimensions,
+// } from 'react-native';
 import { RegistrationScreen } from './Screens/RegistrationScreen/RegistrationScreen';
+import { LoginScreen } from './Screens/LoginScreen/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-SplashScreen.preventAutoHideAsync();
+// // ====================FONTS:===================================
+// SplashScreen.preventAutoHideAsync();
+// // ====================/FONTS:===================================
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Mynerve-Regular': require('./assets/font/Mynerve-Regular.ttf'),
-  });
-
   const onSubmitRegData = ({ name, email, password }) => {
     const RegData = {
       name: name,
@@ -30,60 +30,70 @@ export default function App() {
     };
     console.log(RegData);
   };
+  const AuthStack = createStackNavigator();
+  // // ====================SCREEN WIDTH:===================================
+  // useEffect(() => {
+  //   const onChange = () => {
+  //     const width = Dimensions.get('window').width;
+  //     console.log(width);
+  //   };
+  //   Dimensions.addEventListener('change', onChange);
+  //   return () => {
+  //     Dimensions.removeEventListener('change');
+  //   };
+  // }, []);
+  // // ====================/SCREEN WIDTH:===================================
 
-  useEffect(() => {
-    const onChange = () => {
-      const width = Dimensions.get('window').width;
-      console.log(width);
-    };
-    Dimensions.addEventListener('change', onChange);
-    return () => {
-      Dimensions.removeEventListener('change');
-    };
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
+  // // ====================FONTS:===================================
+  // const [fontsLoaded] = useFonts({
+  //   'Mynerve-Regular': require('./assets/font/Mynerve-Regular.ttf'),
+  // });
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+  // // ====================/FONTS:===================================
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container} onLayout={onLayoutRootView}>
-        <ImageBackground
-          source={require('./assets/BgPic.jpg')}
-          resizeMode="cover"
-          style={styles.BgImg}
-        >
-          <KeyboardAvoidingView
-            behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-          >
-            {/* <LoginScreen /> */}
-            <RegistrationScreen onSubmitRegData={onSubmitRegData} />
-          </KeyboardAvoidingView>
-        </ImageBackground>
-        <StatusBar style="auto" />
-      </View>
-    </TouchableWithoutFeedback>
+    // <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    //   <View style={styles.container} onLayout={onLayoutRootView}>
+    //     <ImageBackground
+    //       source={require('./assets/BgPic.jpg')}
+    //       resizeMode="cover"
+    //       style={styles.BgImg}
+    //     >
+    //       <KeyboardAvoidingView
+    //         behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+    //       >
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="Registration" component={RegistrationScreen} />
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
+    //       </KeyboardAvoidingView>
+    //     </ImageBackground>
+    //     <StatusBar style="auto" />
+    //   </View>
+    // </TouchableWithoutFeedback>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
 
-  BgImg: {
-    flex: 1,
-    // alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//   BgImg: {
+//     flex: 1,
+//     // alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- Screens/RegistrationScreen/RegistrationScreen.jsx
+++ Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import {
+  StyleSheet,
+  View,
   Text,
   TextInput,
-  Button,
   Alert,
   Pressable,
-  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
   Keyboard,
+  ImageBackground,
+  Dimensions,
 } from 'react-native';
+
 import {
   ButtonStyle,
   InputStyle,
@@ -16,6 +27,9 @@ import {
   FormStyle,
   inputTitle,
 } from './RegistrationScreen.styled';
+// ====================FONTS:===================================
+SplashScreen.preventAutoHideAsync();
+// ====================/FONTS:===================================
 
 export const RegistrationScreen = ({ onSubmitRegData }) => {
   const [email, setEmail] = useState('');
@@ -53,52 +67,108 @@ export const RegistrationScreen = ({ onSubmitRegData }) => {
     setName('');
     setPassword('');
   };
+
+  // ====================SCREEN WIDTH:===================================
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get('window').width;
+      console.log(width);
+    };
+    Dimensions.addEventListener('change', onChange);
+    return () => {
+      Dimensions.removeEventListener('change');
+    };
+  }, []);
+  // ====================/SCREEN WIDTH:===================================
+
+  // ====================FONTS:===================================
+  const [fontsLoaded] = useFonts({
+    'Mynerve-Regular': require('../../assets/font/Mynerve-Regular.ttf'),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
+  // ====================/FONTS:===================================
+
   return (
-    <>
-      <View style={FormStyle}>
-        <Text style={HeaderStyle}>Registration</Text>
-        <View>
-          <Text style={inputTitle}>Email</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          source={require('../../assets/BgPic.jpg')}
+          resizeMode="cover"
+          style={styles.BgImg}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+          >
+            <View style={FormStyle}>
+              <Text style={HeaderStyle}>Registration</Text>
+              <View>
+                <Text style={inputTitle}>Email</Text>
 
-          <TextInput
-            placeholder="Write email"
-            value={email}
-            onChangeText={onInputEmail}
-            style={InputStyle}
-            autoFocus
-            textAlign="center"
-          />
-        </View>
-        <View>
-          <Text style={inputTitle}>name</Text>
+                <TextInput
+                  placeholder="Write email"
+                  value={email}
+                  onChangeText={onInputEmail}
+                  style={InputStyle}
+                  autoFocus
+                  textAlign="center"
+                />
+              </View>
+              <View>
+                <Text style={inputTitle}>name</Text>
 
-          <TextInput
-            placeholder="Write name"
-            value={name}
-            onChangeText={onInputName}
-            style={InputStyle}
-            autoFocus
-            textAlign="center"
-          />
-        </View>
-        <View>
-          <Text style={inputTitle}>Password</Text>
-          <TextInput
-            placeholder="Write password"
-            value={password}
-            onChangeText={onInputPassword}
-            style={InputStyle}
-            autoFocus
-            textAlign="center"
-            secureTextEntry
-          />
-        </View>
+                <TextInput
+                  placeholder="Write name"
+                  value={name}
+                  onChangeText={onInputName}
+                  style={InputStyle}
+                  autoFocus
+                  textAlign="center"
+                />
+              </View>
+              <View>
+                <Text style={inputTitle}>Password</Text>
+                <TextInput
+                  placeholder="Write password"
+                  value={password}
+                  onChangeText={onInputPassword}
+                  style={InputStyle}
+                  autoFocus
+                  textAlign="center"
+                  secureTextEntry
+                />
+              </View>
 
-        <Pressable onPress={onRegistration} style={ButtonStyle}>
-          <Text style={ButtonTextStyle}>Register</Text>
-        </Pressable>
-        {/* <Button title="Register" onPress={onRegistration} style={ButtonStyle} /> */}
+              <Pressable onPress={onRegistration} style={ButtonStyle}>
+                <Text style={ButtonTextStyle}>Register</Text>
+              </Pressable>
+              {/* <Button title="Register" onPress={onRegistration} style={ButtonStyle} /> */}
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+        <StatusBar style="auto" />
       </View>
-    </>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+
+  BgImg: {
+    flex: 1,
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
